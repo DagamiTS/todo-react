11a0abde59c4d888730eba570d88a9ef0ed9c8f2
Denis Efremov <dagamitheslayer@gmail.com>
2020-07-06 18:01:41 +0300

    Updates

---
+++ precache-manifest.8dc83c9de27ccec29dfa1e5ec0c21d32.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1dd8fdb2b8b8197416730cfcd9f6e9f",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "23d15a56d468058b938d",
+    "url": "/todo-react/static/css/main.10eb8ebc.chunk.css"
+  },
+  {
+    "revision": "a0dd80b9b560f9027158",
+    "url": "/todo-react/static/js/2.59a38a4e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/todo-react/static/js/2.59a38a4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23d15a56d468058b938d",
+    "url": "/todo-react/static/js/main.c3c3caf0.chunk.js"
+  },
+  {
+    "revision": "a049eaa3d15bfcc71b2e",
+    "url": "/todo-react/static/js/runtime-main.b204df10.js"
+  },
+  {
+    "revision": "0ad8e57ba97bd9c95e36ba18a4da9c12",
+    "url": "/todo-react/static/media/add.0ad8e57b.svg"
+  },
+  {
+    "revision": "14e2f379ea2f16993535086cc7b87b36",
+    "url": "/todo-react/static/media/close.14e2f379.svg"
+  },
+  {
+    "revision": "77e1ba27b305d29dbfb7f872f83335c2",
+    "url": "/todo-react/static/media/edit.77e1ba27.svg"
+  },
+  {
+    "revision": "a980d6ac98ddb42e3a03aeb1bb3059bb",
+    "url": "/todo-react/static/media/remove.a980d6ac.svg"
+  }
+]);
